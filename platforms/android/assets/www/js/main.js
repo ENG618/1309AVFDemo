9e4971a2eff5344d47c22cc1ddbe3a5fab44575a
eng618 <eng618@fullsail.edu>
2013-09-30 22:31:12 -0400

    Minor changes on program closes

--- platforms/android/assets/www/js/main.js
+++ platforms/android/assets/www/js/main.js
@@ -7,6 +7,7 @@
 // Global Variables
 var pictureSource;   // picture source
 var destinationType; // sets the format of returned value
+var Connection;  // network connection
 
 // Global Functions
 
@@ -136,21 +137,39 @@ var popularFn = function(){
     }// End if/else to check network connection
 };// End Display Popular Photos Function
 
+
+var weatherData = function(recieved){
+    alert('yay!!');
+};
+
+
+
+
 // Current Weather Function
 var currentFn = function(){
-    var zipCode = $('#zipSearch').val();
-    console.log(zipCode);
-    if (zipCode === "" ){
-        alert('Please enter a zip code you would like to know the weather for');
-    }else{
-        confirm('Please confirm "' + zipCode + '" is the zip code you would like to search for');
-        var url = 'https://api.weathersource.com/v1/a8136057c6beafe4e9b8/history_by_postal_code.json?_callback=weatherData&period=day&postal_code_eq=' + zipCode + '&country_eq=US&timestamp_eq=2012-02-10T00:00:00-05:00&fields=postal_code,country,timestamp,tempMax,tempAvg,tempMin,precip,snowfall,windSpdMax,windSpdAvg,windSpdMin,cldCvrMax,cldCvrAvg,cldCvrMin,dewPtMax,dewPtAvg,dewPtMin,feelsLikeMax,feelsLikeAvg,feelsLikeMin,relHumMax,relHumAvg,relHumMin,sfcPresMax,sfcPresAvg,sfcPresMin,spcHumMax,spcHumAvg,spcHumMin,wetBulbMax,wetBulbAvg,wetBulbMin';
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+
+    // Geo Success/Error Functions
+    function geoSuccess(position) {
+
+        alert('Current temp: 68.8 degrees');
+        var url = 'https://api.weathersource.com/v1/a8136057c6beafe4e9b8/history.json?_callback=geoData&latitude_eq=' + position.coords.latitude + '&longitude_eq=' + position.coords.longitude + '&period=hour&timestamp_eq=2004-02-12T08:00&fields=temp';
         $.ajax({
             url: url,
             type: 'GET',
             dataType: 'jsonp',
             success: function(received){
-                console.log(received);
+                alert('test');
             }
         })
         .done(function() {
@@ -162,6 +181,45 @@ var currentFn = function(){
         .always(function() {
             console.log("complete");
         });
+
+        // Need code here to display location based data
+
+
+    }
+
+    // onError Callback receives a PositionError object
+    //
+    function geoError(error) {
+        alert('message: ' + error.message + '\n');
+    }  // End Geo Success/Error Functions
+
+    if (states[networkState] === 'No network connection') {
+        alert('Please connect to the internet to access this function');
+    }else{
+        var zipCode = $('#zipSearch').val();
+        if (zipCode === "" ){
+            navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+        }else{
+            confirm('Please confirm "' + zipCode + '" is the zip code you would like to search for');
+            var zipUrl = 'https://api.weathersource.com/v1/a8136057c6beafe4e9b8/history_by_postal_code.json?_callback=weatherData&period=day&postal_code_eq=' + zipCode + '&country_eq=US&timestamp_eq=2012-02-10T00:00:00-05:00&fields=postal_code,country,timestamp,tempMax,tempAvg,tempMin,precip,snowfall,windSpdMax,windSpdAvg,windSpdMin,cldCvrMax,cldCvrAvg,cldCvrMin,dewPtMax,dewPtAvg,dewPtMin,feelsLikeMax,feelsLikeAvg,feelsLikeMin,relHumMax,relHumAvg,relHumMin,sfcPresMax,sfcPresAvg,sfcPresMin,spcHumMax,spcHumAvg,spcHumMin,wetBulbMax,wetBulbAvg,wetBulbMin';
+            $.ajax({
+                url: zipUrl,
+                type: 'GET',
+                dataType: 'jsonp',
+                success: function(received){
+                    alert(received);
+                }
+            })
+            .done(function() {
+                console.log("success");
+            })
+            .fail(function() {
+                console.log("error");
+            })
+            .always(function() {
+                console.log("complete");
+            });
+        }
     }
 };// End Current Weather Function
 
@@ -216,28 +274,22 @@ var connectionFn = function(){
     states[Connection.CELL]     = 'Cell generic connection';
     states[Connection.NONE]     = 'No network connection';
 
-    if (states[networkState] === 'No network connection') {
-        alert('Please connect to the internet to access this function');
-    }else{
-        alert('Connection type: ' + states[networkState]);
-    }
+    alert('Connection type: ' + states[networkState]);
 };// End Check Connection
 
 // Check Geo Location
 var geoFn = function(){
     function geoSuccess(position) {
-        alert('Latitude: '          + position.coords.latitude          + '\n' +
-          'Longitude: '         + position.coords.longitude         + '\n' +
-          'Altitude: '          + position.coords.altitude          + '\n' +
-          'Accuracy: '          + position.coords.accuracy          + '\n' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-          'Heading: '           + position.coords.heading           + '\n' +
-          'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');
-
-
+        alert(
+            'Latitude: '          + position.coords.latitude          + '\n' +
+            'Longitude: '         + position.coords.longitude         + '\n' +
+            'Altitude: '          + position.coords.altitude          + '\n' +
+            'Accuracy: '          + position.coords.accuracy          + '\n' +
+            'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+            'Heading: '           + position.coords.heading           + '\n' +
+            'Speed: '             + position.coords.speed             + '\n' +
+            'Timestamp: '         + position.timestamp                + '\n');
     }
-
     // onError Callback receives a PositionError object
     //
     function geoError(error) {
@@ -267,13 +319,13 @@ function onDeviceReady(){
     destinationType=navigator.camera.DestinationType;
 
 
-    $("#searchBtn").on("click", searchFn);
-    $("#popular").on("click", popularFn);
-    $("#currentBtn").on("click", currentFn);
-    $("#snapPic").on("click", picFn);
-    $("#connection").on("click", connectionFn);
-    $("#geoBtn").on("click", geoFn);
-    $("#notify").on("click", notifyFn);
+$("#searchBtn").on("click", searchFn);
+$("#popular").on("click", popularFn);
+$("#currentBtn").on("click", currentFn);
+$("#snapPic").on("click", picFn);
+$("#connection").on("click", connectionFn);
+$("#geoBtn").on("click", geoFn);
+$("#notify").on("click", notifyFn);
 }
 
 document.addEventListener("deviceready", onDeviceReady, false);
